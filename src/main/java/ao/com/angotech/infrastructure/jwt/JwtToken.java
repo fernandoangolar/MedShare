8bc2350767a37a13739574f3f5b05e3e89b6a360
Fernando Angolar <fernandoangolar581@gmail.com>
2025-07-23 15:08:29 +0100

    cfeat: onfigurar Spring Security e utilitário JWT

---
+++ src/main/java/ao/com/angotech/infrastructure/jwt/JwtToken.java
@@ -0,0 +1,20 @@
+package ao.com.angotech.infrastructure.jwt;
+
+public class JwtToken {
+
+    private String token;
+
+    public JwtToken() {}
+
+    public JwtToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
